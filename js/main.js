f02471afead4f00030c2470b47d6707d1d9f0484
Skumarcibm <63644198+Skumarcibm@users.noreply.github.com>
2021-05-25 19:11:43 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal();
+sr.reveal('.showcase-left', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+sr.reveal('.showcase-right', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px'
+});
+
+sr.reveal('#animabout', {
+    duration: 1000,
+    delay: 500
+});
+sr.reveal('#user-img-nb', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
